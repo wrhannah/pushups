cad9a5d14b07dd539798a68ede0307a488c282a0
wrhannah <wrhannah@users.noreply.github.com>
2025-01-08 09:05:53 -0500

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -40,7 +40,7 @@ def home():
 def start_scheduler():
     scheduler = BackgroundScheduler()
     # Schedule the job to run every weekday at 5:00 AM
-    scheduler.add_job(send_email, "cron", day_of_week="mon-fri", hour=13, minute=59)
+    scheduler.add_job(send_email, "cron", day_of_week="mon-fri", hour=14, minute=8)
     scheduler.start()
     print("Scheduler started!")
 
